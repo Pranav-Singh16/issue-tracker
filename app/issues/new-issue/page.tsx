b9f5e3ce5c5edac21c27d49d77015050f0424a73
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-06-30 22:35:36 +0530

    form submit with post db

--- app/issues/new-issue/page.tsx
+++ app/issues/new-issue/page.tsx
@@ -3,38 +3,45 @@
 import { Button, TextField } from "@radix-ui/themes";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 // 👇 dynamically import SimpleMDE
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssue = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-5">
-      <TextField.Root placeholder="Title" />
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-5"
+      onSubmit={handleSubmit(async (data) => {
+        console.log(data);
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit</Button>
-    </div>
+    </form>
   );
 };
 
 export default NewIssue;
-
-// "use client";
-
-// import { Button, TextField } from "@radix-ui/themes";
-// import SimpleMDE from "react-simplemde-editor";
-// import "easymde/dist/easymde.min.css";
-
-// const newIssue = () => {
-//   return (
-//     <div className="max-w-xl space-y-5">
-//       <TextField.Root placeholder="Title" />
-
-//       <SimpleMDE placeholder="Description" />
-//       <Button>Submit</Button>
-//     </div>
-//   );
-// };
-// export default newIssue;
