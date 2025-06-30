28ed2aad7e28fded5852429c6465adc27120ac82
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-06-30 21:56:01 +0530

    fix for markdown

--- app/issues/new-issue/page.tsx
+++ app/issues/new-issue/page.tsx
@@ -1,16 +1,40 @@
 "use client";
 
 import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 
-const newIssue = () => {
+// 👇 dynamically import SimpleMDE
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+const NewIssue = () => {
   return (
     <div className="max-w-xl space-y-5">
-      <TextField.Root placeholder="Title"></TextField.Root>
+      <TextField.Root placeholder="Title" />
       <SimpleMDE placeholder="Description" />
       <Button>Submit</Button>
     </div>
   );
 };
-export default newIssue;
+
+export default NewIssue;
+
+// "use client";
+
+// import { Button, TextField } from "@radix-ui/themes";
+// import SimpleMDE from "react-simplemde-editor";
+// import "easymde/dist/easymde.min.css";
+
+// const newIssue = () => {
+//   return (
+//     <div className="max-w-xl space-y-5">
+//       <TextField.Root placeholder="Title" />
+
+//       <SimpleMDE placeholder="Description" />
+//       <Button>Submit</Button>
+//     </div>
+//   );
+// };
+// export default newIssue;
