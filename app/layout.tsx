8bd55ccf42946139d412564fde279be5bb8008ce
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-06-30 18:13:02 +0530

    set up redix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import "@radix-ui/themes/styles.css";
 import "./globals.css"; // or wherever your global styles are
 import { Inter } from "next/font/google";
+import { Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 // ✅ Initialize the font
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
