f4d21d0dc72ffb92ab0cc2ed229342c5ecfef92c
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-07-04 23:54:31 +0530

    skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,25 @@
-import Spinner from "@/app/components/Spinner";
+import IssueStatusBatch from "@/app/components/IssueStatusBatch";
+import { Heading, Flex, Card } from "@radix-ui/themes";
+import Markdown from "react-markdown";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const IssueLoading = () => {
   return (
-    <div>
-      <Spinner />
+    <div className="max-w-xl">
+      <Heading>
+        <Skeleton />
+      </Heading>
+      <Flex gap="3">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose">
+        <Skeleton count={3} />
+      </Card>
+      <p>
+        <Skeleton width="5rem" />
+      </p>
     </div>
   );
 };
--- app/issues/new-issue/loading.tsx
+++ app/issues/new-issue/loading.tsx
@@ -1,9 +1,12 @@
 import Spinner from "@/app/components/Spinner";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const IssueLoading = () => {
   return (
-    <div>
-      <Spinner />
+    <div className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
     </div>
   );
 };
