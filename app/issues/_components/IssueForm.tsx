20c9a1c0453280dccdf2545336a6de37281ee9f2
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-07-06 19:40:51 +0530

    api patch

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,7 +8,7 @@ import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
-import { createIssueSchema } from "@/app/validationSchema";
+import { issueSchema } from "@/app/validationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { ErrorMessage, Spinner } from "@/app/components/index";
@@ -19,7 +19,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false, // This ensures the component is only rendered on the client side
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
@@ -30,7 +30,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     setValue, // Get setValue from RHF
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
@@ -94,94 +94,3 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 };
 
 export default IssueForm;
-
-// "use client";
-
-// import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-// import dynamic from "next/dynamic";
-// import "easymde/dist/easymde.min.css";
-// import { useForm, Controller } from "react-hook-form";
-// import axios from "axios";
-// import { useRouter } from "next/navigation";
-// import { useState } from "react";
-// import { createIssueSchema } from "@/app/validationSchema";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { z } from "zod";
-// import { ErrorMessage, Spinner } from "@/app/components/index";
-// import { Issue } from "@prisma/client";
-
-// // 👇 dynamically import SimpleMDE
-// const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-//   ssr: false,
-// });
-
-// type IssueFormData = z.infer<typeof createIssueSchema>;
-
-// const IssueForm = ({ issue }: { issue?: Issue }) => {
-//   const router = useRouter();
-//   const {
-//     register,
-//     control,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm<IssueFormData>({
-//     resolver: zodResolver(createIssueSchema),
-//   });
-//   const [error, setError] = useState("");
-//   const [isSubmitting, setSubmitting] = useState(false);
-
-//   const onSubmit = handleSubmit(async (data) => {
-//     console.log(data);
-//     setSubmitting(true);
-//     try {
-//       await axios.post("/api/issues", data);
-//       // await new Promise((resolve) => setTimeout(resolve, 5000));
-
-//       router.push("/issues");
-//     } catch (error) {
-//       setError("unexpected error occured");
-//     } finally {
-//       setSubmitting(false); // Always reset the submitting state
-//     }
-//   });
-
-//   console.log(issue?.description);
-
-//   return (
-//     <div className="max-w-xl ">
-//       {error && (
-//         <Callout.Root color="red" className="mb-5">
-//           <Callout.Text>{error}</Callout.Text>
-//         </Callout.Root>
-//       )}
-
-//       <form className="space-y-5" onSubmit={onSubmit}>
-//         <TextField.Root
-//           defaultValue={issue?.title}
-//           placeholder="Title"
-//           {...register("title")}
-//         />
-//         <ErrorMessage>{errors.title?.message}</ErrorMessage>
-//         <Controller
-//           name="description"
-//           control={control}
-//           render={({ field }) => (
-//             <SimpleMDE
-//               defaultValue={issue?.description}
-//               placeholder="Description"
-//               {...field}
-//             />
-//           )}
-//         />
-//         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
-//         <Button disabled={isSubmitting}>
-//           Submit
-//           {isSubmitting && <Spinner />}
-//         </Button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default IssueForm;
