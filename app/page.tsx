8ece9d8bc0830a95ec718bb1ba56808a4b8a2bdb
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-07-25 23:46:34 +0530

    pagination add and subtract with query

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div>hello world</div>;
+import Pagination from "./components/Pagination";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const params = await searchParams;
+  const currentPage = parseInt(params.page || "1") || 1;
+
+  return <Pagination itemCount={100} pageSize={10} currentPage={currentPage} />;
 }
+
+// import Pagination from "./components/Pagination";
+
+// export default function Home({
+//   searchParams,
+// }: {
+//   searchParams: { page: string };
+// }) {
+//   return (
+//     <Pagination
+//       itemCount={100}
+//       pageSize={10}
+//       currentPage={parseInt(searchParams.page)}
+//     />
+//   );
+// }
