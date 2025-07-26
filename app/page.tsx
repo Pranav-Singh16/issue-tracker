13a7476a112d1981383bf25e8566d11a96633ccf
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-07-26 14:49:54 +0530

    latestissue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,7 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ page?: string }>;
-}) {
-  const params = await searchParams;
-  const currentPage = parseInt(params.page || "1") || 1;
-
-  return <Pagination itemCount={100} pageSize={10} currentPage={currentPage} />;
+export default function Home() {
+  return (
+    <LatestIssues />
+  );
 }
-
-// import Pagination from "./components/Pagination";
-
-// export default function Home({
-//   searchParams,
-// }: {
-//   searchParams: { page: string };
-// }) {
-//   return (
-//     <Pagination
-//       itemCount={100}
-//       pageSize={10}
-//       currentPage={parseInt(searchParams.page)}
-//     />
-//   );
-// }
